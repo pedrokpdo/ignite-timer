10985f90b69594d8dfa207050001b12ad9c99147
pedrokpdo <pedromotoehm@gmail.com>
2022-07-09 15:03:24 -0300

    header e layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,17 @@
-export function Header () {
-    return <h1>HEADER</h1>
+import { HeaderContainer } from "./styles";
+import logoIgnite from '../../assets/logo-ignite.svg'
+import {Timer, Scroll} from 'phosphor-react'
+import { NavLink } from "react-router-dom";
+
+
+export function Header() {
+    return (
+        <HeaderContainer>
+            <img src={logoIgnite} alt="" />
+            <nav>
+                <NavLink to="/"><Timer size={24}/></NavLink>
+                <NavLink to="/history"><Scroll size={24}/></NavLink>
+            </nav>
+        </HeaderContainer>
+    )
 }
